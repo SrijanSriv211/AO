b49bef1573fff8c758af4e078bab40b56df5c1b9
SrijanSriv211 <QCoreNest@gmail.com>
2024-08-17 22:19:35 +0530

    Create precompiled header

---
+++ src/aospch.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <filesystem>
+#include <functional>
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <cctype>
+#include <format>
+#include <string>
+#include <chrono>
+#include <cmath>
+#include <regex>
+#include <map>
+
+#include <windows.h>
