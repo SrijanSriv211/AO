8feaf1ce63daf56f359cf091662f33a46ec82f93
SrijanSriv211 <QCoreNest@gmail.com>
2024-08-19 17:47:55 +0530

    Add --init command to initialize AO in cwd

--- src/core/entrypoint/entrypoint.cpp
+++ src/core/entrypoint/entrypoint.cpp
@@ -3,12 +3,14 @@
 
 #include "argparse/argparse.h"
 #include "fileio/foldersystem.h"
+#include "fileio/filesystem.h"
 #include "console/console.h"
 
 int take_entry(const std::vector<std::string> args)
 {
     argparse parser = argparse("AO", "A developer tool made by a developer for developers", unrecognized_argument_error);
     parser.add({"-h", "--help", "/?", "-?"}, "Show help message", "", true, false);
+    parser.add({"-i", "--init"}, "Initialize AO in current directory", "", true, false);
 
     std::vector<argparse::parsed_argument> parsed_args = parser.parse(args);
 
@@ -34,6 +36,5 @@ void unrecognized_argument_error(const std::string& err)
 void init_folders()
 {
     foldersystem::create(".ao");
-    foldersystem::create(".ao/root");
-    foldersystem::create(".ao/etc");
+    filesystem::write(".as\\ao.json", "{}\n");
 }
