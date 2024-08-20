8f1d89e62e572c56eb627eb93ed70dbddffff5eb
SrijanSriv211 <QCoreNest@gmail.com>
2024-08-20 17:55:04 +0530

    Optimize compile time by hashing files, comparing them then recompiling them if needed

--- scripts/build.py
+++ scripts/build.py
@@ -1,39 +1,31 @@
 from utils import *
 import sys, os
 
-config.src_path = "src"
-config.icon_path = "src/ico.o"
-config.includes = ["src/", "src/shared/", "src/vendor/"]
-config.defines = ["VERSION=2024.1"]
-config.std = "c++20"
-config.outpath = "bin\\AO.exe"
-config.filedata = [
+CONFIG["SRC_PATH"] = "src"
+CONFIG["ICON_PATH"] = "src/ico.o"
+CONFIG["INCLUDES"] = ["src/", "src/shared/", "src/vendor/"]
+CONFIG["DEFINES"] = ["VERSION=2024.1"]
+CONFIG["STD"] = "c++20"
+CONFIG["OUTPATH"] = "bin\\AO.exe"
+CONFIG["PRECOMPILES"] = [
     ("src/aopch.h.gch", "g++ src/aopch.h"), # compile AO precompiled headers
     ("src/ico.o", "windres src/ico.rc -O coff -o src/ico.o") # create ico.o containing the data for AO icon
 ]
 
-if not os.path.isdir("bin"):
-    os.mkdir("bin")
-
-for i, x in config.filedata:
-    if not os.path.isfile(i):
-        precompile_files()
-        break
-
 if not sys.argv[1:]:
-    config.optimization = "-O2"
+    CONFIG["OPTIMIZATION"] = "-O2"
     precompile_files()
     compile_ao()
 
 for i, x in enumerate(sys.argv[1:]):
     if x == "clean":
-        rm(sys.argv[i+2:] if sys.argv[i+2:] else ["bin/", "src/aopch.h.gch", "src/ico.o"])
+        rm(sys.argv[i+2:] if sys.argv[i+2:] else ["bin/", "obj/", "src/aopch.h.gch", "src/ico.o"])
 
     elif x == "pch":
         precompile_files()
 
     elif x == "run":
-        config.optimization = ""
+        CONFIG["OPTIMIZATION"] = ""
         compile_ao()
         input("press enter to continue.")
         run_ao(sys.argv[i+2:])
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,17 +1,18 @@
-import shutil, time, os
+import hashlib, shutil, time, os
 
-class config:
-    src_path = "src"
-    icon_path = "src/ico.o"
-    includes = ["src/", "src/shared/"]
-    defines = ["VERSION=1"]
-    std = "c++20"
-    optimization = "-O2"
-    outpath = "bin/AO.exe"
-    filedata = [
+CONFIG = {
+    "SRC_PATH": "src",
+    "ICON_PATH": "src/ico.o",
+    "INCLUDES": ["src/", "src/shared/"],
+    "DEFINES": ["VERSION=1"],
+    "STD": "c++20",
+    "OPTIMIZATION": "-O2",
+    "OUTPATH": "bin/AO.exe",
+    "PRECOMPILES": [
         ("src/aopch.h.gch", "g++ src/aopch.h"), # compile AO precompiled headers
         ("src/ico.o", "windres src/ico.rc -O coff -o src/ico.o") # create ico.o containing the data for AO icon
     ]
+}
 
 # convert seconds to hours, minutes and seconds
 def sec_to_time(seconds):
@@ -31,7 +32,13 @@ def sec_to_time(seconds):
         f"{seconds} second" if seconds == 1 else f"{seconds} seconds" if seconds > 0 else ""
     ]
 
-    print("total time: " + ", ".join(t))
+    total_time = ", ".join(t)
+    total_time = total_time if total_time else "0"
+
+    print("total time: " + total_time)
+
+def mkdirs(*dirs):
+    [os.mkdir(dir) for dir in dirs if not os.path.isdir(dir)]
 
 def rm(files):
     for i in files:
@@ -44,21 +51,84 @@ def rm(files):
 def join(lst, prefix="", separator=" "):
     return separator.join([prefix + i for i in lst])
 
-# https://stackoverflow.com/a/2909998/18121288
-def get_src_paths(path, ext):
-    return [os.path.join(path, f"*{ext}") for path, _, files in os.walk(path) if any(name.endswith(ext) for name in files)]
-
 def precompile_files():
-    for path, cmd in config.filedata:
+    for path, cmd in CONFIG["PRECOMPILES"]:
         if not os.path.isfile(path):
             os.system(cmd)
 
-def compile_ao():
-    script = f"g++ {config.icon_path} {join(get_src_paths(config.src_path, "cpp"))} {join(config.includes, "-I")} {join(config.defines, "-D")} {config.optimization} -Wall -std={config.std} -o {config.outpath}"
+def get_files(path: str, ext):
+    src = []
 
+    for path, _, files in os.walk(path):
+        src.extend([os.path.join(path, file) for file in files if file.endswith(ext)])
+
+    return src
+
+def create_unique_name(filepath: str):
+    return filepath.replace("/", "_").replace("\\", "_").replace(":", "_")
+
+def hash_file(file):
+    md5_h = hashlib.md5()
+
+    with open(file, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            md5_h.update(chunk)
+
+    return md5_h.hexdigest()
+
+def save_hash(file, hash_value):
+    unique_name = create_unique_name(file)
+    hash_file = os.path.join("obj", unique_name + ".hash")
+
+    with open(hash_file, 'w') as f:
+        f.write(hash_value)
+
+def load_hash(file):
+    unique_name = create_unique_name(file)
+    hash_file = os.path.join("obj", unique_name + ".hash")
+
+    if os.path.exists(hash_file):
+        with open(hash_file, 'r') as f:
+            return f.read().strip()
+
+    return None
+
+def delete_stale_objects():
+    valid_hashes = {create_unique_name(f) + ".hash" for f in get_files(CONFIG["SRC_PATH"], "cpp")}
+    obj_hashes = {i.replace("obj\\", "") for i in get_files("obj", "hash")}
+
+    for i in list(obj_hashes - valid_hashes):
+        obj_name = i.replace(".hash", ".o")
+        rm(["obj\\" + i, "obj\\" + obj_name])
+
+################################################################################################################################
+###################################################### CODE TO COMPILE AO ######################################################
+################################################################################################################################
+
+def COMMON():
+    return f"{join(CONFIG["INCLUDES"], "-I")} {join(CONFIG["DEFINES"], "-D")} {CONFIG["OPTIMIZATION"]} -std={CONFIG["STD"]}"
+
+def compile_to_obj(file):
+    unique_name = create_unique_name(file)
+    obj_file = os.path.join("obj", unique_name + ".o")
+    current_hash = hash_file(file)
+    previous_hash = load_hash(file)
+
+    if current_hash != previous_hash or not os.path.isfile(obj_file):
+        os.system(f"g++ -c {file} {COMMON()} -o {obj_file}")
+        save_hash(file, current_hash)
+
+    return obj_file
+
+def compile_ao():
     start = time.perf_counter()
-    os.system(script)
+
+    mkdirs("bin", "obj")
+    delete_stale_objects()
+    obj_files = [compile_to_obj(f) for f in get_files(CONFIG["SRC_PATH"], "cpp")]
+    os.system(f"g++ {CONFIG["ICON_PATH"]} {join(obj_files)} {COMMON()} -Wall -o {CONFIG["OUTPATH"]}")
+
     sec_to_time(time.perf_counter() - start)
 
 def run_ao(args):
-    os.system(f"{config.outpath} {join(args)}")
+    os.system(f"{CONFIG["OUTPATH"]} {join(args)}")
