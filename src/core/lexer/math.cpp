6959b404c47f948d6a22a97fe9d4ee45f841ea21
SrijanSriv211 <QCoreNest@gmail.com>
2024-08-25 16:12:00 +0530

    Start with basics of math evaluation

--- src/core/lexer/math.cpp
+++ src/core/lexer/math.cpp
@@ -3,15 +3,18 @@
 
 #include "strings/strings.h"
 
-void lex::math(std::string& expression)
+std::string lex::math(const std::string& expression)
 {
-    // std::regex re(R"([\+\-\*/]+|\d*\.?\d+|[\(\)])");
-    // std::regex re(R"([-+*/()]+|\d+(?:_\d+)*\.?\d*)");
-    // expression = strings::replace_all(expression, "_", "");
-    // std::vector<std::string> toks = this->tokenizer(expression, re);
+    std::string expr = strings::replace_all(expression, "_", "");
+    expr = strings::replace_all(expr, " ", "");
+
+    std::regex re(R"([-+*/()]+?|\d*\.?\d+)");
+    std::vector<std::string> toks = this->tokenizer(expr, re);
 
     // for (std::vector<std::string>::size_type i = 0; i < toks.size(); i++)
     // {
     //     std::cout << "(" << toks[i] << ")\n";
     // }
+
+    return expr;
 }
